642f80b75a13e8a0a2565a6cd68a5104daa2e776
Nikola Rusakov <rusakov.nikola@gmail.com>
2023-07-26 11:45:39 +0200

    feat: bar chart poc

--- example/lib/pages/interval.dart
+++ example/lib/pages/interval.dart
@@ -20,193 +20,193 @@ class IntervalPage extends StatelessWidget {
         child: Center(
           child: Column(
             children: <Widget>[
-              Container(
-                padding: const EdgeInsets.fromLTRB(20, 40, 20, 5),
-                child: const Text(
-                  'Interactive Bar Chart',
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(10, 5, 10, 0),
-                alignment: Alignment.centerLeft,
-                child: const Text(
-                  '- A tooltip and crosshair on selection.',
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(10, 5, 10, 0),
-                alignment: Alignment.centerLeft,
-                child: const Text(
-                  '- Bar colors and shadow elevations change with selection state.',
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(10, 5, 10, 0),
-                alignment: Alignment.centerLeft,
-                child: const Text(
-                  '- Double tap to clear the selection.',
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(top: 10),
-                width: 350,
-                height: 300,
-                child: Chart(
-                  data: basicData,
-                  variables: {
-                    'genre': Variable(
-                      accessor: (Map map) => map['genre'] as String,
-                    ),
-                    'sold': Variable(
-                      accessor: (Map map) => map['sold'] as num,
-                    ),
-                  },
-                  marks: [
-                    IntervalMark(
-                      label: LabelEncode(
-                          encoder: (tuple) => Label(tuple['sold'].toString())),
-                      elevation: ElevationEncode(value: 0, updaters: {
-                        'tap': {true: (_) => 5}
-                      }),
-                      color:
-                          ColorEncode(value: Defaults.primaryColor, updaters: {
-                        'tap': {false: (color) => color.withAlpha(100)}
-                      }),
-                    )
-                  ],
-                  axes: [
-                    Defaults.horizontalAxis,
-                    Defaults.verticalAxis,
-                  ],
-                  selections: {'tap': PointSelection(dim: Dim.x)},
-                  tooltip: TooltipGuide(),
-                  crosshair: CrosshairGuide(),
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(20, 40, 20, 5),
-                child: const Text(
-                  'Transposed Bar Chart',
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(10, 5, 10, 0),
-                alignment: Alignment.centerLeft,
-                child: const Text(
-                  '- Uses gradient encode instead of color.',
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(top: 10),
-                width: 350,
-                height: 300,
-                child: Chart(
-                  data: basicData,
-                  variables: {
-                    'genre': Variable(
-                      accessor: (Map map) => map['genre'] as String,
-                    ),
-                    'sold': Variable(
-                      accessor: (Map map) => map['sold'] as num,
-                    ),
-                  },
-                  marks: [
-                    IntervalMark(
-                      label: LabelEncode(
-                          encoder: (tuple) => Label(tuple['sold'].toString())),
-                      gradient: GradientEncode(
-                          value: const LinearGradient(colors: [
-                            Color(0x8883bff6),
-                            Color(0x88188df0),
-                            Color(0xcc188df0),
-                          ], stops: [
-                            0,
-                            0.5,
-                            1
-                          ]),
-                          updaters: {
-                            'tap': {
-                              true: (_) => const LinearGradient(colors: [
-                                    Color(0xee83bff6),
-                                    Color(0xee3f78f7),
-                                    Color(0xff3f78f7),
-                                  ], stops: [
-                                    0,
-                                    0.7,
-                                    1
-                                  ])
-                            }
-                          }),
-                    )
-                  ],
-                  coord: RectCoord(transposed: true),
-                  axes: [
-                    Defaults.verticalAxis
-                      ..line = Defaults.strokeStyle
-                      ..grid = null,
-                    Defaults.horizontalAxis
-                      ..line = null
-                      ..grid = Defaults.strokeStyle,
-                  ],
-                  selections: {'tap': PointSelection(dim: Dim.x)},
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(20, 40, 20, 5),
-                child: const Text(
-                  'Interval Bar Chart',
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(10, 5, 10, 0),
-                alignment: Alignment.centerLeft,
-                child: const Text(
-                  '- Make sure to specify a same scale for all variables in a same dimension.',
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(10, 5, 10, 0),
-                alignment: Alignment.centerLeft,
-                child: const Text(
-                  '- With corner radius.',
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(top: 10),
-                width: 350,
-                height: 300,
-                child: Chart(
-                  data: intervalData,
-                  variables: {
-                    'id': Variable(
-                      accessor: (Map map) => map['id'] as String,
-                    ),
-                    'min': Variable(
-                      accessor: (Map map) => map['min'] as num,
-                      scale: LinearScale(min: 0, max: 160),
-                    ),
-                    'max': Variable(
-                      accessor: (Map map) => map['max'] as num,
-                      scale: LinearScale(min: 0, max: 160),
-                    ),
-                  },
-                  marks: [
-                    IntervalMark(
-                      position: Varset('id') * (Varset('min') + Varset('max')),
-                      shape: ShapeEncode(
-                          value: RectShape(
-                              borderRadius: BorderRadius.circular(2))),
-                    )
-                  ],
-                  axes: [
-                    Defaults.horizontalAxis,
-                    Defaults.verticalAxis,
-                  ],
-                ),
-              ),
+              // Container(
+              //   padding: const EdgeInsets.fromLTRB(20, 40, 20, 5),
+              //   child: const Text(
+              //     'Interactive Bar Chart',
+              //     style: TextStyle(fontSize: 20),
+              //   ),
+              // ),
+              // Container(
+              //   padding: const EdgeInsets.fromLTRB(10, 5, 10, 0),
+              //   alignment: Alignment.centerLeft,
+              //   child: const Text(
+              //     '- A tooltip and crosshair on selection.',
+              //   ),
+              // ),
+              // Container(
+              //   padding: const EdgeInsets.fromLTRB(10, 5, 10, 0),
+              //   alignment: Alignment.centerLeft,
+              //   child: const Text(
+              //     '- Bar colors and shadow elevations change with selection state.',
+              //   ),
+              // ),
+              // Container(
+              //   padding: const EdgeInsets.fromLTRB(10, 5, 10, 0),
+              //   alignment: Alignment.centerLeft,
+              //   child: const Text(
+              //     '- Double tap to clear the selection.',
+              //   ),
+              // ),
+              // Container(
+              //   margin: const EdgeInsets.only(top: 10),
+              //   width: 350,
+              //   height: 300,
+              //   child: Chart(
+              //     data: basicData,
+              //     variables: {
+              //       'genre': Variable(
+              //         accessor: (Map map) => map['genre'] as String,
+              //       ),
+              //       'sold': Variable(
+              //         accessor: (Map map) => map['sold'] as num,
+              //       ),
+              //     },
+              //     marks: [
+              //       IntervalMark(
+              //         label: LabelEncode(
+              //             encoder: (tuple) => Label(tuple['sold'].toString())),
+              //         elevation: ElevationEncode(value: 0, updaters: {
+              //           'tap': {true: (_) => 5}
+              //         }),
+              //         color:
+              //             ColorEncode(value: Defaults.primaryColor, updaters: {
+              //           'tap': {false: (color) => color.withAlpha(100)}
+              //         }),
+              //       )
+              //     ],
+              //     axes: [
+              //       Defaults.horizontalAxis,
+              //       Defaults.verticalAxis,
+              //     ],
+              //     selections: {'tap': PointSelection(dim: Dim.x)},
+              //     tooltip: TooltipGuide(),
+              //     crosshair: CrosshairGuide(),
+              //   ),
+              // ),
+              // Container(
+              //   padding: const EdgeInsets.fromLTRB(20, 40, 20, 5),
+              //   child: const Text(
+              //     'Transposed Bar Chart',
+              //     style: TextStyle(fontSize: 20),
+              //   ),
+              // ),
+              // Container(
+              //   padding: const EdgeInsets.fromLTRB(10, 5, 10, 0),
+              //   alignment: Alignment.centerLeft,
+              //   child: const Text(
+              //     '- Uses gradient encode instead of color.',
+              //   ),
+              // ),
+              // Container(
+              //   margin: const EdgeInsets.only(top: 10),
+              //   width: 350,
+              //   height: 300,
+              //   child: Chart(
+              //     data: basicData,
+              //     variables: {
+              //       'genre': Variable(
+              //         accessor: (Map map) => map['genre'] as String,
+              //       ),
+              //       'sold': Variable(
+              //         accessor: (Map map) => map['sold'] as num,
+              //       ),
+              //     },
+              //     marks: [
+              //       IntervalMark(
+              //         label: LabelEncode(
+              //             encoder: (tuple) => Label(tuple['sold'].toString())),
+              //         gradient: GradientEncode(
+              //             value: const LinearGradient(colors: [
+              //               Color(0x8883bff6),
+              //               Color(0x88188df0),
+              //               Color(0xcc188df0),
+              //             ], stops: [
+              //               0,
+              //               0.5,
+              //               1
+              //             ]),
+              //             updaters: {
+              //               'tap': {
+              //                 true: (_) => const LinearGradient(colors: [
+              //                       Color(0xee83bff6),
+              //                       Color(0xee3f78f7),
+              //                       Color(0xff3f78f7),
+              //                     ], stops: [
+              //                       0,
+              //                       0.7,
+              //                       1
+              //                     ])
+              //               }
+              //             }),
+              //       )
+              //     ],
+              //     coord: RectCoord(transposed: true),
+              //     axes: [
+              //       Defaults.verticalAxis
+              //         ..line = Defaults.strokeStyle
+              //         ..grid = null,
+              //       Defaults.horizontalAxis
+              //         ..line = null
+              //         ..grid = Defaults.strokeStyle,
+              //     ],
+              //     selections: {'tap': PointSelection(dim: Dim.x)},
+              //   ),
+              // ),
+              // Container(
+              //   padding: const EdgeInsets.fromLTRB(20, 40, 20, 5),
+              //   child: const Text(
+              //     'Interval Bar Chart',
+              //     style: TextStyle(fontSize: 20),
+              //   ),
+              // ),
+              // Container(
+              //   padding: const EdgeInsets.fromLTRB(10, 5, 10, 0),
+              //   alignment: Alignment.centerLeft,
+              //   child: const Text(
+              //     '- Make sure to specify a same scale for all variables in a same dimension.',
+              //   ),
+              // ),
+              // Container(
+              //   padding: const EdgeInsets.fromLTRB(10, 5, 10, 0),
+              //   alignment: Alignment.centerLeft,
+              //   child: const Text(
+              //     '- With corner radius.',
+              //   ),
+              // ),
+              // Container(
+              //   margin: const EdgeInsets.only(top: 10),
+              //   width: 350,
+              //   height: 300,
+              //   child: Chart(
+              //     data: intervalData,
+              //     variables: {
+              //       'id': Variable(
+              //         accessor: (Map map) => map['id'] as String,
+              //       ),
+              //       'min': Variable(
+              //         accessor: (Map map) => map['min'] as num,
+              //         scale: LinearScale(min: 0, max: 160),
+              //       ),
+              //       'max': Variable(
+              //         accessor: (Map map) => map['max'] as num,
+              //         scale: LinearScale(min: 0, max: 160),
+              //       ),
+              //     },
+              //     marks: [
+              //       IntervalMark(
+              //         position: Varset('id') * (Varset('min') + Varset('max')),
+              //         shape: ShapeEncode(
+              //             value: RectShape(
+              //                 borderRadius: BorderRadius.circular(2))),
+              //       )
+              //     ],
+              //     axes: [
+              //       Defaults.horizontalAxis,
+              //       Defaults.verticalAxis,
+              //     ],
+              //   ),
+              // ),
               Container(
                 padding: const EdgeInsets.fromLTRB(20, 40, 20, 5),
                 child: const Text(
@@ -252,6 +252,9 @@ class IntervalPage extends StatelessWidget {
                     'index': Variable(
                       accessor: (Map map) => map['index'].toString(),
                     ),
+                    'label': Variable(
+                      accessor: (Map map) => map['label'].toString(),
+                    ),
                     'type': Variable(
                       accessor: (Map map) => map['type'] as String,
                     ),
@@ -263,7 +266,7 @@ class IntervalPage extends StatelessWidget {
                   marks: [
                     IntervalMark(
                       position:
-                          Varset('index') * Varset('value') / Varset('type'),
+                          Varset('label') * Varset('value') / Varset('type'),
                       shape: ShapeEncode(value: RectShape(labelPosition: 0.5)),
                       color: ColorEncode(
                           variable: 'type', values: Defaults.colors10),
@@ -271,7 +274,7 @@ class IntervalPage extends StatelessWidget {
                           encoder: (tuple) => Label(
                                 tuple['value'].toString(),
                                 LabelStyle(
-                                    textStyle: const TextStyle(fontSize: 6)),
+                                    textStyle: const TextStyle(fontSize: 8)),
                               )),
                       modifiers: [StackModifier()],
                     )
